8131fc290cde87764d29e73f43b85f6f976168dc
theAbanjoy <aban.the.se@gmail.com>
2025-05-20 16:49:53 +0530

    Initial Commit

---
+++ tenant-1/src/main/java/com/incede/gitTry/Tenant1Application.java
@@ -0,0 +1,13 @@
+package com.incede.gitTry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tenant1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tenant1Application.class, args);
+	}
+
+}
